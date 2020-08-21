6cd5b3dd18b5538d3cd1fd7de8aa55fc93d93e98
Chris <chris.gonzales.82@gmail.com>
2020-08-21 15:42:13 -0700

    Created project, gitignore, and ReadMe

---
+++ ARGameOfLife/ARGameOfLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGameOfLife
+//
+//  Created by Chris Gonzales on 8/21/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
